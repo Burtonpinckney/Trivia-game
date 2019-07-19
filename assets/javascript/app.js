d6aa54aaa8d53da8c9e7a4fea99ce6eb25355d72
Burton Pinckney <mbpinckney@gmail.com>
2019-07-19 00:50:32 -0400

    Did initial set up, html and js incomplete

---
+++ assets/javascript/app.js
@@ -0,0 +1,25 @@
+var questions = "";
+var answerChoices = "";
+var answerKey = "";
+var userAnswers = "";
+var correct = 0;
+var incorrect = 0;
+
+$("startBut").on("click", function() {
+
+    var Timer = setTimeout(myFunction, 30000);
+
+function createQuiz(questions, answerChoices) {
+    var answerChoice1 = ["Jonathan Stewart", "Stephen Davis", "DeAngelo Williams", "Christian McCaffrey"];
+
+
+
+
+    $("#restart").on("click"), function() {
+
+    }
+}
+});
+
+
+
