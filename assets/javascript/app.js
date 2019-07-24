d959585e4121b92f3cff5ac9585150ac0a61db54
Burton Pinckney <mbpinckney@gmail.com>
2019-07-24 16:39:33 -0400

    Updated JS and finish version 1.0

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,26 +1,70 @@
-var questions = "";
-var answerChoices = "";
-var answerKey = "";
+var answerKey = ["1", "2", "4"];
 var userAnswers = "";
-var correct = 0;
-var incorrect = 0;
+var correct
+var incorrect
+var runClock
+$("#startBut").on("click", function() {
+    var count = 16
+    runClock=true
+    correct = 0
+    incorrect = 0
+    $("#timesUp").css("display", "none");
+    $(".results").css("display", "none");
+    $(".questions").css("display", "block");
+    $("#startBut").css("display", "none");
+    $("#timer").css("display", "block");
+    
+    // setTimeout(timer, 30000);
+    // setTimeout(timeUp, 0);
 
-$("startBut").on("click", function() {
-
-    var timer = setTimeout(gradeQuiz, 30000);
-    $("#timer").append(timer);
-
-function createQuiz(questions, answerChoices) {
-    var answerChoice1 = ["Jonathan Stewart", "Stephen Davis", "DeAngelo Williams", "Christian McCaffrey"];
-
-
-
-
-    $("#restart").on("click"), function() {
-
+    function timer() {
+        count --
+        if (count === 0){
+            $("#timesUp").css("display", "block");
+            check()
+        }
+    
+        else{
+            $("#time").text(count);
+            if(runClock){
+            setTimeout(timer,1000)}
+        }
+        
     }
-}
+
+    timer();
+
+    
+
+
+    
+
+//     $("#timer").append(timer);
+
+// function createQuiz(questions, answerChoices) {
+//     var answerChoice1 = ["Jonathan Stewart", "Stephen Davis", "DeAngelo Williams", "Christian McCaffrey"];
+// }
 });
 
 
-
+$("#submit").on("click", check);
+function check() {
+    runClock = false
+    $(".questions").css("display", "none");
+    $(".results").css("display", "block");
+    $("#timer").css("display", "none");
+    $("#startBut").css("display", "block");
+    for (let i = 0; i < answerKey.length; i++) {
+        if (answerKey[i] === $("#q"+i).val()){
+            console.log("correct on",i+1)
+            correct++;
+        }
+        else{
+            console.log("wrong on ",i+1)
+            incorrect++;
+        }
+    }
+    $(".correct").text(correct);
+    $(".incorrect").text(incorrect);
+    console.log("correct=",correct,"incorrect",incorrect)
+}
